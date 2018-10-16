ba089919227822bcb3b0b04e5a8c91b120aad2d2
Gabriel Martins da Silva <gms.1997@hotmail.com>
2018-10-16 19:36:23 -0300

    Prototype

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    Rigidbody rg;
+    public float torque;
+
+	// Use this for initialization
+	void Start () {
+		rg=gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rg.AddTorque(transform.up*torque*Random.Range(-1.0f,1.0f));
+	}
+}
---
+++ Assets/Scripts/SpawnCube.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCube : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		
+		var pos = new Vector3(Random.Range(70f,450f),17f, 20f);
+		other.transform.position = pos;
+		
+	}
+	
+}
