92d0a4bccc3c96b0629e688d847fe2df2d7c443e
Gabriel Martins da Silva <gms.1997@hotmail.com>
2018-10-20 01:53:44 -0300

    Colisão da Bala e Limpeza dos Códigos de Controle
    Bala colide com cenário, Códigos de Controle separados em funções (Update mais limpo), player atira bala mesmo, Raycast ficou BEM bosta

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private Rigidbody rb;
+	private string parentName;
+	public float speed = 500;
+	
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Mover a bala
+	void Update () {
+		// Mover a bala em direção e velocidade constante
+		//transform.position += transform.forward * Time.deltaTime * speed;
+		rb.MovePosition(transform.position + transform.forward * Time.deltaTime * speed);
+	}
+
+	// Colisão
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.name != parentName){
+			if(other.gameObject.tag == "Player"){
+				Destroy(other.gameObject); // usar método Death quando existir
+			}
+			Destroy(this.gameObject);
+		}
+	}
+
+	public void SetParentName(string name){
+		parentName = name;
+	}
+}
--- Assets/Scripts/ControleTeclado.cs
+++ Assets/Scripts/ControleTeclado.cs
@@ -6,40 +6,54 @@ public class ControleTeclado : MonoBehaviour {
 
 	private Rigidbody rb;
 	private float timeToShoot;
+	public float moveSpeed = 100f;
 	public GameObject bullet;
 	public Color bulletColor;
-	public float shotDelay = 0.5f;
+	public float shotDelay = 1f;
 	
 	void Start(){
 		timeToShoot = 0;
 		rb = GetComponent<Rigidbody>();
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		// Mover personagem
-		var h = Input.GetAxis("Horizontal");
-        var v = Input.GetAxis("Vertical");
-		var movement = new Vector3(-v*100f, 0f, h*100f);
-		transform.Translate(movement*Time.deltaTime, Space.World);
-
-		// Limites de cenário (melhorar)
-		rb.position = new Vector3
-		(
-			Mathf.Clamp (rb.position.x, 70, 450),
-			rb.position.y,
-			Mathf.Clamp (rb.position.z, 10, 489)
-		);
+		Move();
 		
 		// Virar personagem
+		Turn();
+
+		// Atirar
+		Shoot();
+		
+	}
+
+	// Mover personagem
+	void Move(){
+		var leftX = Input.GetAxis("Horizontal");
+        var leftY = Input.GetAxis("Vertical");
+		var movement = new Vector3(-leftY, 0f, leftX);
+		rb.MovePosition(rb.position + movement*moveSpeed*Time.deltaTime);
+
+		// Limites de cenário (hardcoded)
+		rb.position = new Vector3(Mathf.Clamp (rb.position.x, 70, 450),
+									rb.position.y,
+									Mathf.Clamp (rb.position.z, 10, 489));
+	}
+
+	// Virar personagem
+	void Turn(){
 		var rZ = Input.GetAxis("Horizontal_2");
         var rX = Input.GetAxis("Vertical_2");
 		var direction = new Vector3(-rX, 0, rZ);
 		if(direction != Vector3.zero)
 			rb.transform.forward = direction;
+	}
 
+	// Atirar com a arma
+	void Shoot(){
 		// Tempo entre tiros
 		if(timeToShoot > 0) 
 			timeToShoot -= Time.deltaTime;
@@ -47,9 +61,19 @@ public class ControleTeclado : MonoBehaviour {
 		// Atirar
 		if(Input.GetButtonDown("Fire1") && timeToShoot <= 0){
 			var shot = Instantiate(bullet, transform.position, transform.rotation);
-			shot.GetComponent<PlayerBullet>().SetParentName(this.name);
+			shot.GetComponent<Bullet>().SetParentName(this.name);
 			shot.GetComponent<MeshRenderer>().material.color = bulletColor;
 			timeToShoot = shotDelay;
 		}
+	}
+
+	// Arremessar Granada
+	void Grenade(){
+		
+	}
+
+	// Invulnerabilidade
+	void Invulnerable(){
+		
 	}
 }
