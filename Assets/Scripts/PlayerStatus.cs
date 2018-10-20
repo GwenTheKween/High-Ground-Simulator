71cc694538781192e64866f37f744175e89859f6
Unknown <augustopaulo123@hotmail.com>
2018-10-20 14:25:17 -0300

    Morte Player
    Morte funcionando e diminuindo pontuação do player

---
+++ Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatus : MonoBehaviour {
+
+	private Rigidbody rb;
+	public int score;
+	public float lowGroundZ;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	public void Death(){
+		if(transform.position.z > lowGroundZ)
+		{
+			score -= 1;
+			if (score < 0)
+				score = 0;
+			rb.MovePosition(new Vector3(rb.position.x,rb.position.y,15));
+		}
+	}
+}
