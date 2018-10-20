53f499c7012fc76df91aee4f48598c5591ee1d61
Unknown <augustopaulo123@hotmail.com>
2018-10-20 15:09:15 -0300

    Player Status
    Um script guardando informações importantes do player

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 public class PlayerStatus : MonoBehaviour {
 
 	private Rigidbody rb;
-	public int score;
+	private int score;
+	private float highGroundCounter;
 	public float lowGroundZ;
+	public float highGroundZ;
+	public bool isProtected;
+	public float highGroundTime;
 
 	void Start(){
 		rb = GetComponent<Rigidbody>();
+		isProtected = false;
+		score = 0;
 	}
 	
+	public void IncreaseScore(int value = 1){
+		score += value;
+	}
+
+	public int getScore(){
+		return score;
+	}
+
 	public void Death(){
-		if(transform.position.z > lowGroundZ)
+		if(transform.position.z > lowGroundZ && !isProtected)
 		{
 			score -= 1;
 			if (score < 0)
 				score = 0;
 			rb.MovePosition(new Vector3(rb.position.x,rb.position.y,15));
 		}
+	}
+
+	public bool isOnHighGround(){
+		if (transform.position.z >= highGroundZ){
+			return true;
+		}
+		return false;
 	}
 }
