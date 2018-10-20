94b728fa969d67ef0f8518e9a7ba5824713e80ac
Gabriel Martins da Silva <gms.1997@hotmail.com>
2018-10-20 15:54:33 -0300

    Inicio da lógica

---
+++ Assets/Highground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highground : MonoBehaviour {
+
+	private List<PlayerStatus> kings;
+
+	// Use this for initialization
+	void Start () {
+		kings = new List<PlayerStatus>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Player entrou no Highground
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Player"){
+			kings.Add(other.gameObject.GetComponent<PlayerStatus>());
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == "Player"){
+			
+		}
+	}
+}
