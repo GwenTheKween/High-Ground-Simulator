1ccc710bd49724f4f9ac47fde8d3db8c5ef418db
billionai <larsen.bruno.p@gmail.com>
2018-10-25 21:32:35 -0300

    Controle do volume da BGM pode ser controlado pelo menu principal

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    private AudioSource AS;
+
+	// Use this for initialization
+	void Start () {
+        AS = GetComponent<AudioSource>();
+        AS.volume = VolumeScript.bgm;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
