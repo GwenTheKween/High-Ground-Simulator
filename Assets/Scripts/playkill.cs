006cd79fd412347b0230b6e665a25df2b1b5cb75
Chieppe <chieppe2142@gmail.com>
2018-10-27 15:14:19 -0200

    Mapa pronto

---
+++ Assets/Scripts/playkill.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playkill : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider col) {
+        var ps = col.gameObject.GetComponent<PlayerStatus>();
+        if(ps) { 
+            ps.Death();
+        }
+    }
+}
