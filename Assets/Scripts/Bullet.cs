f9c528ce61be2a80510358e1c2fc92d1b8a1ab18
billionai <larsen.bruno.p@gmail.com>
2018-10-31 20:59:54 -0300

    Fixed not being able to shoot on/through high ground

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -23,7 +23,7 @@ public class Bullet : MonoBehaviour {
 
 	// Colisão
 	void OnTriggerEnter(Collider other){
-		if(other.gameObject.name != parentName){
+		if(other.gameObject.name != parentName && other.gameObject.tag != "Region"){
 			if(other.gameObject.tag == "Player" && !other.GetComponent<PlayerStatus>().isProtected){
 				//Destroy(other.gameObject); // usar método Death quando existir
 				other.gameObject.GetComponent<PlayerStatus>().Death();
