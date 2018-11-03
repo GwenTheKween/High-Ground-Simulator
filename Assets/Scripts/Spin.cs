f747fbf818eb40f2a7813f9cca91406fe21699ae
Gabriel Martins da Silva <gms.1997@hotmail.com>
2018-11-03 17:34:23 -0300

    Modelo da bomba e ícone do jogo

---
+++ Assets/Scripts/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+
+	public float speedX = 100f;
+	public float speedY = 100f;
+	public float speedZ = 100f;
+	
+	void Update () {
+		transform.Rotate(new Vector3(1f,0f,0f), speedX * Time.deltaTime);
+		transform.Rotate(new Vector3(0f,1f,0f), speedY * Time.deltaTime);
+		transform.Rotate(new Vector3(0f,0f,1f), speedZ * Time.deltaTime);
+	}
+}
