4b5d053fc31bbd63720222b1972fbc92b845d55d
Amador <amador_interativa@hotmail.com>
2018-11-06 19:39:24 -0200

    Pequeno Ajuste

--- Assets/Scripts/BulletBomb.cs
+++ Assets/Scripts/BulletBomb.cs
@@ -38,7 +38,7 @@ public class BulletBomb : MonoBehaviour {
 	// Colisão
 	void OnTriggerEnter(Collider other){
 		if(other.gameObject.name != parentName && other.gameObject.tag != "Detector" && other.gameObject.tag != "Region"
-												 && other.gameObject.tag != "MapLimit"){
+												 && other.gameObject.tag != "MapLimit" && other.gameObject.tag != "Turret"){
 			Explode();
 		}
 	}
